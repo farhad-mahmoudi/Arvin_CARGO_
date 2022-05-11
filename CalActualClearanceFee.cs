3059b91b5f54a1556976fc56d26f4ab7026829ee
Farhad Mahmoudi <55744315+farhad-mahmoudi@users.noreply.github.com>
2022-05-12 03:39:33 +0430

    Add files via upload

---
+++ CalActualClearanceFee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Cargo
+{
+   public class CalActualClearanceFee :CodeActivity
+    {
+        [RequiredArgument]
+        [Input("محموله ")]
+        [ReferenceTarget("new_cargo")]
+        public InArgument<EntityReference> CargoIn { get; set; }
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            EntityReference cargoRef = CargoIn.Get(executionContext);
+            Entity cargoEntity = service.Retrieve(cargoRef.LogicalName, cargoRef.Id,
+                new ColumnSet("new_totalclearancefee", "new_totalsuppliedprice"));
+            //هزینه ترخیص واقعی محموله
+            decimal totalClearanceFee = 0;
+            if (cargoEntity.Contains("new_totalclearancefee") == true)
+            {
+                totalClearanceFee = (decimal)cargoEntity["new_totalclearancefee"];
+            }
+
+            //مجموع قیمت تامین شده 
+            decimal totalSupplierPrice = 0;
+            if (cargoEntity.Contains("new_totalsuppliedprice") == true)
+            {
+                totalSupplierPrice = (decimal)cargoEntity["new_totalsuppliedprice"];
+            }
+
+            //ردیف های  محموله
+            QueryExpression qe = new QueryExpression("new_cargorow");
+            qe.Criteria.AddCondition("new_relatedcargo", ConditionOperator.Equal, cargoRef.Id);
+            qe.ColumnSet = new ColumnSet("new_totalsuppliedprice");
+            EntityCollection cargoRows = service.RetrieveMultiple(qe);
+
+            foreach (var row in cargoRows.Entities)
+            {
+                //هزینه تامین شده کل
+                decimal totalSupplierPriceRow = 0;
+                if (row.Contains("new_totalsuppliedprice") == true)
+                {
+                    totalSupplierPriceRow = (decimal)row["new_totalsuppliedprice"];
+                }
+                //هزینه ترخیص واقعی 
+                decimal actualClearenceFee = 0;
+                if (totalSupplierPrice != 0 && totalSupplierPriceRow != 0 && totalClearanceFee != 0)
+                {
+                    actualClearenceFee = (totalClearanceFee * totalSupplierPriceRow) / totalSupplierPrice;
+                }
+
+                Entity cargoRow = new Entity("new_cargorow", row.Id);
+                cargoRow["new_actualclearancefee"] = actualClearenceFee;
+
+                service.Update(cargoRow);
+            }
+
+
+        }
+    }
+}
---
+++ CalActualShippingCost.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Cargo
+{
+    public class CalActualShippingCost : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("محموله ")]
+        [ReferenceTarget("new_cargo")]
+        public InArgument<EntityReference> CargoIn { get; set; }
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            EntityReference cargoRef = CargoIn.Get(executionContext);
+            Entity cargoEntity = service.Retrieve(cargoRef.LogicalName, cargoRef.Id,
+                new ColumnSet("new_totalshippingcost", "new_totalactualweight"));
+            //هزینه حمل واقعی محموله
+            decimal totalShippingCost = 0;
+            if (cargoEntity.Contains("new_totalshippingcost") == true)
+            {
+                totalShippingCost = (decimal)cargoEntity["new_totalshippingcost"];
+            }
+
+            //مجموع وزن واقعی
+            decimal totalActualWeight = 0;
+            if (cargoEntity.Contains("new_totalactualweight") == true)
+            {
+                totalActualWeight = (decimal)cargoEntity["new_totalactualweight"];
+            }
+            //ردیف های  محموله
+            QueryExpression qe = new QueryExpression("new_cargorow");
+            qe.Criteria.AddCondition("new_relatedcargo", ConditionOperator.Equal, cargoRef.Id);
+            qe.ColumnSet = new ColumnSet("new_totalactualweight");
+            EntityCollection cargoRows = service.RetrieveMultiple(qe);
+
+            foreach (var row in cargoRows.Entities)
+            {
+                //وزن واقعی کل
+                decimal totalActualWeightRow = 0;
+                if (row.Contains("new_totalactualweight") == true)
+                {
+                    totalActualWeightRow = (decimal)row["new_totalactualweight"];
+                }
+                //هزینه حمل واقعی
+                decimal actualShippingCost = 0;
+                if (totalActualWeight != 0 && totalActualWeightRow != 0 && totalShippingCost != 0)
+                {
+                    actualShippingCost = (totalShippingCost * totalActualWeightRow) / totalActualWeight;
+                }
+
+                Entity cargoRow = new Entity("new_cargorow", row.Id);
+                cargoRow["new_actualshippingcost"] = actualShippingCost;
+
+                service.Update(cargoRow);
+            }
+
+
+        }
+    }
+}
